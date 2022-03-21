294956287d9bb28083a35e6434fc71732cb3f5f9
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2022-03-20 21:32:39 -0400

    feat: dashboard and transactions table components

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,6 +1,12 @@
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export const Dashboard = () => {
-    return (<Container><Summary /></Container>);
-}
+  return (
+    <Container>
+      <Summary />
+      <TransactionsTable />
+    </Container>
+  );
+};
